a6fb3b4ffc0b093770846192b743bc6a2f65f15e
Shimosora <shimo_sora@outlook.jp>
2017-06-15 18:16:11 +0900

    Initial commit

---
+++ app/src/main/java/org/t_robop/shimomura/zikannwari/zikanwariActivity.java
@@ -0,0 +1,13 @@
+package org.t_robop.shimomura.zikannwari;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class zikanwariActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zikanwari);
+    }
+}
